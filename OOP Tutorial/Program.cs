a8a568529e40493ce76de2934ec47de7efaf101b
leigh <leigh@DESKTOP-7TPRGE9>
2020-02-07 12:15:34 -0500

    Uploaded example of polymorphism using base class Animal

--- OOP Tutorial/Program.cs
+++ OOP Tutorial/Program.cs
@@ -4,12 +4,56 @@ namespace OOP_Tutorial
 {
     class Car
     {
-        string color = "red";
+        string color;
+        int maxSpeed;
 
         static void Main (string[] args)
         {
             Car myObj = new Car();
+            myObj.color = "red";
+            myObj.maxSpeed = 200;
             Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+
         }
     }
+
+    class Animal  // Parent class 
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Pig : Animal  //Child class 
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Dog : Animal  //Child class
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();  //Create Animal object
+            Animal myPig = new Pig();  //Create Pig object
+            Animal myDog = new Dog();  //Create Dog object
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+        }
+    }
+
 }
