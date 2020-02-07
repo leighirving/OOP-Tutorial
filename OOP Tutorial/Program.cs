e74b116b03b6982dffb8b42ce5803c61ef2c5f47
leigh <leigh@DESKTOP-7TPRGE9>
2020-02-07 11:28:45 -0500

    Created Car class and object

---
+++ OOP Tutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOP_Tutorial
+{
+    class Car
+    {
+        string color = "red";
+
+        static void Main (string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
